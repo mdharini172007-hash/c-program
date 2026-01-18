67e4370cb4f5dcabffa53e2fa0ce0a9c31991d9e
Harini D <mdharini172007@gmail.com>
2026-01-18 22:38:55 +0530

    Add files via upload

---
+++ ClimbingStairs.c
@@ -0,0 +1,15 @@
+int climbStairs(int n) {
+    if (n <= 2) return n;
+
+    int first = 1;
+    int second = 2;
+    int ways = 0;
+
+    for (int i = 3; i <= n; i++) {
+        ways = first + second;
+        first = second;
+        second = ways;
+    }
+
+    return ways;
+}
