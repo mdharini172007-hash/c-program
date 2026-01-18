d704d8664dffe07f6cc549fee42295f2270dabe1
Harini D <mdharini172007@gmail.com>
2026-01-18 22:40:25 +0530

    Add files via upload

---
+++ plusone.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+
+char* addBinary(char* a, char* b) {
+    int lenA = strlen(a);
+    int lenB = strlen(b);
+    int maxLen = (lenA > lenB ? lenA : lenB) + 2; // +1 for carry, +1 for '\0'
+
+    char *res = (char*)malloc(maxLen * sizeof(char));
+    res[maxLen - 1] = '\0';
+
+    int i = lenA - 1;
+    int j = lenB - 1;
+    int k = maxLen - 2;
+    int carry = 0;
+
+    while (i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+        if (i >= 0) sum += a[i--] - '0';
+        if (j >= 0) sum += b[j--] - '0';
+
+        res[k--] = (sum % 2) + '0';
+        carry = sum / 2;
+    }
+
+    // If result starts with leading zero, move pointer
+    return res + k + 1;
+}#include <stdlib.h>
+
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int carry = 1;  // we are adding 1
+    int *res = (int*)malloc((digitsSize + 1) * sizeof(int));
+
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        int sum = digits[i] + carry;
+        res[i + 1] = sum % 10;
+        carry = sum / 10;
+    }
+
+    if (carry) {
+        res[0] = carry;
+        *returnSize = digitsSize + 1;
+        return res;
+    } else {
+        // shift array to remove leading zero
+        for (int i = 0; i < digitsSize; i++)
+            res[i] = res[i + 1];
+        *returnSize = digitsSize;
+        return res;
+    }
+}
